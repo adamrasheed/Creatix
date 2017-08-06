5bcb7f912f54a5d2dc81e5907153a10685bb0ff4
Adam Rasheed <adamrasheed91@gmail.com>
2017-08-05 19:33:53 -0700

    improved home about section

--- site/snippets/home/about.php
+++ site/snippets/home/about.php
@@ -1,10 +1,12 @@
 <div class="row row--center">
-    <div class="section-about sml med--col-6 med--offset-1 lrg--col-8">
+    <div class="section-about__container sml med--col-6 med--offset-1 lrg--col-8">
     <?php if($pic = image('headshot.jpg')->url()): ?>
-        <img src="<?php echo $pic ?>" alt="Adam Rasheed" class="section-about__img">
+        <div class="section-about__img-container">
+            <img src="<?php echo $pic ?>" alt="Adam Rasheed" class="section-about__img">
+        </div>
     <?php endif; ?>
         <div class="section-about__content">
-            <h3>Are you trying to reach a bigger audience online?</h3>
+            <h3 class="section-about__title">Are you trying to reach a bigger audience online?</h3>
             <p>Hi, I'm Adam Rasheed, a Web Designer who helps Small Businesses like yours with custom websites designed around their business goals.</p>
                 <a href="<?php echo $site->page('about')->url() ?>" class="inline-block">Learn How I Can Help <i class="fa fa-angle-right" aria-hidden="true"></i></a>
         </div><!-- end content -->
